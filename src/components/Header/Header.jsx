6f0aed9d042c3020761251a378c399bf24812552
Touqeer Ahmad <touqeer.ahmad@grocerapps.com>
2022-06-12 23:08:00 +0500

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Container, Nav, Navbar, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './Header.scss'
+
+const Header = () => {
+    return (
+        <div className='Header_part'>
+            <Navbar  collapseOnSelect expand="lg" bg="light" variant="light">
+                <Container>
+                    <Navbar.Brand href="/">
+                        <img className='brand_logo' src="assets/images/brand-logo.svg" alt="Chearful" />
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse className='d-flex align-items-center' id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Link className='link_item' to="/">Explore</Link>
+                            <Link className='link_item' to="/">About</Link>
+                            <Link className='link_item' to="/">Contact us</Link>
+                            <Link className='link_item' to="/">Placeholder</Link>
+                        </Nav>
+                        <Nav className='d-flex align-items-center'>
+                            <Button className='language_btn' variant='outline-dark'>
+                                <img className='me-1' src='assets/images/flag.png' alt='UAE Flag' /> <img src='assets/images/arrow_down.svg' alt='arrow down' />
+                            </Button>
+                            <Button className='language_btn' variant='outline-dark'>
+                                <img src='assets/images/language-icon.svg' alt='Language' />En <img src='assets/images/arrow_down.svg' alt='arrow down' />
+                            </Button>
+                            <Button className='action_button custom_outlined' variant='outline-dark'>Sign up</Button>
+                            <Button className='action_button text-white'>Log in</Button>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Header } from '../../components';
+import './Home.scss'
+
+const Home = () => {
+    return (
+        <div className='home_page'>
+            <h1>Home page</h1>
+        </div>
+    )
+}
+
+export default Home;
